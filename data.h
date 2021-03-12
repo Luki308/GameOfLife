6a21be25585da2cde5b8895f96365ae803a42cbd
Patryk Chojnicki <01153132@pw.edu.pl>
2021-03-12 18:50:00 +0100

    Minor stylistic change in data.h

--- data.h
+++ data.h
@@ -10,8 +10,8 @@ typedef struct w {
     char **cells; 
 } *world_t;
 
-world_t alloc_world(int rows, int columns);
 void free_world(world_t world);
+world_t alloc_world(int rows, int columns);
 world_t create_world(char *filename);
 void print_world(world_t world, FILE *output, bool size_info);
 void store_world(world_t world, char *filename);
