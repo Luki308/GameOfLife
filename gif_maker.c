b896d65d18dd2ca259fa2ec59ffd0700d03c9b99
Patryk Chojnicki <01153132@pw.edu.pl>
2021-03-13 01:08:49 +0100

    Fixed error with scaling and opposite dimensions

--- gif_maker.c
+++ gif_maker.c
@@ -11,7 +11,7 @@ void start_gif(char* name, int rows, int columns, int given_scale, int given_del
     delay = given_delay;
     gif = ge_new_gif(
         name,   // name of output gif file
-        scale * rows, scale * columns,  // scaled up dimensions
+        scale * columns, scale * rows,  // scaled up dimensions
         (uint8_t []) {  // palette
             0xFF, 0xFF, 0xFF,   // 0 -> white
             0x00, 0x00, 0x00,   // 1 -> black
@@ -25,11 +25,13 @@ void paint_frame(world_t world)
 {
     for(int r = 0; r < scale * world->rows; r++)
         for(int c = 0; c < scale * world->columns; c++)
-            gif->frame[r * world->rows + c] = world->cells[r/scale][c/scale];
+            gif->frame[r * scale * world->columns + c] = world->cells[r/scale][c/scale];
     ge_add_frame(gif, delay);
 }
 
 void finish_gif()
 {
+    printf("%d\n",scale);
+    printf("%d\n",delay);
     ge_close_gif(gif);
 }
